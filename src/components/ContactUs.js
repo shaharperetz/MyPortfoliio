01503c7af1a70a2738330243b1037cee631a8d8b
shahar <shaharp2@gmail.com>
2020-06-15 19:27:59 +0300

    shahar

---
+++ src/components/ContactUs.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+export default class ContactUs extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="contact">
+        <div className="row section-head">
+          <div className="ten columns">
+            <p className="lead">
+              Feel free to contact me for any work or suggestions below
+              </p>
+          </div>
+        </div>
+        <div className="row">
+          <aside className="eigth columns footer-widgets">
+            <div className="widget">
+              <h4>
+                shaharp2@gmail.com
+              </h4>
+              <h4>052-7799694</h4>
+            </div>
+          </aside>
+        </div>
+      </section>
+    );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,75 @@
+let resumeData = {
+  "imagebaseurl": "https://rbhatia46.github.io/",
+  "name": "Shahar peretz",
+  "role": "Fullstack Developer",
+  "linkedinId": "Your LinkedIn Id",
+  "skypeid": "Your skypeid",
+  "roleDescription": "Specializing in advanced frameworks, including React, VueJS, Angular, NodeJS, MongoDB and more",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/shahar-p-662a791a9/",
+      "className": "fa fa-linkedin"
+    }
+
+  ],
+  "aboutme": "Full-Stack / Frontend  Web Developer with knowledge and experience writing single-page-applications Using the latest WEB technologies such as Node.js, React.js , redux , Vue.js ,  and Angular A team player, hard worker, focusing on problem solving and detailed orientation, self-learner responsible, and adapt quickly to new environments.",
+  "address": "Israel , Tel-Aviv 052-7799694",
+  "education": [
+    {
+      "UniversityName": "Coding-Academy 640 hours bootcamp",
+      "specialization": "	An intensive coding bootcamp (640 hours).",
+      "MonthOfPassing": "March",
+      "YearOfPassing": "2020",
+      "Achievements": "Finished the boot-camp successfully"
+    },
+
+  ],
+  "skillsDescription": "Your skills here",
+  "skills": [
+    {
+      "skillname": "HTML5"
+    },
+    {
+      "skillname": "JavaScript"
+    },
+    {
+      "skillname": "CSS"
+    },
+    {
+      "skillname": "Reactjs"
+    },
+    {
+      "skillname": "MongoDB"
+    },
+    {
+      "skillname": "NodeJS"
+    },
+    {
+      "skillname": "Angular"
+    },
+    {
+      "skillname": "Vuejs"
+    },
+  ],
+  "portfolio": [
+    {
+      "name": "Sunday",
+      "description": "A mangament tasks project inspired by monday.com ",
+      "imgurl": "images/sunday.png"
+    },
+
+  ],
+  "testimonials": [
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    },
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
