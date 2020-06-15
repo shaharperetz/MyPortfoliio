51841b69a5955b83153ab1d1f9b48ac931efbf01
shahar <shaharp2@gmail.com>
2020-06-15 19:39:03 +0300

    Updates

---
+++ precache-manifest.e03199d3dfe5fb78c5a3c56a57065e29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f5790bdeb26699ae445",
+    "url": "/MyPortfolii/static/css/main.784c0731.chunk.css"
+  },
+  {
+    "revision": "3f5790bdeb26699ae445",
+    "url": "/MyPortfolii/static/js/main.3f5790bd.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/MyPortfolii/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "3e1269f12131c16e99e6",
+    "url": "/MyPortfolii/static/js/runtime~main.3e1269f1.js"
+  },
+  {
+    "revision": "040c0f90283b06fc2362ab6b148eeba6",
+    "url": "/MyPortfolii/index.html"
+  }
+];
